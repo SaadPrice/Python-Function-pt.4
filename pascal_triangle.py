9933b0768ce98ee0b106343a3ad45851a7b51fa7
Saad <educationalmindz@gmail.com>
2024-04-30 23:08:11 -0400

    pascal_triangel.py.

---
+++ pascal_triangle.py
@@ -0,0 +1,17 @@
+def pascal(n):
+    triangle = []
+    for i in range(n):
+        row = [1] * (i + 1)
+        if i >= 2:
+            for j in range(1, i):
+                row[j] = triangle[i - 1][j - 1] + triangle[i - 1][j]
+        triangle.append(row)
+
+    for row in triangle:
+        print(" ".join(map(str, row)))
+
+# Testing the function with provided examples
+print("pascal(1):")
+pascal(1)
+print("\npascal(5):")
+pascal(5)
