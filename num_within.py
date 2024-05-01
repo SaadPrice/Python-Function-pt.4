82536cdc2eb7f593b57d900a783aad3807b31059
Saad <educationalmindz@gmail.com>
2024-04-30 23:00:40 -0400

    num_within.py created.

---
+++ num_within.py
@@ -0,0 +1,3 @@
+#This function will take a number and a range (inclusive) as input and return True if the number falls within the given range, otherwise False.
+def num_within(num, start, end):
+    return start <= num <= end
